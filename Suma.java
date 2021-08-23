325e24e5eb6c3951cce8f64fa8e50d64e0c73c8b
DiegoSebastian04 <diego.ccasillas@alumnos.udg.mx>
2021-08-23 17:11:48 -0500

    Update Suma.java

--- Suma.java
+++ Suma.java
@@ -7,10 +7,18 @@ public class Suma{
 		int num1=0;
 		int num2=2;
 		System.out.println("Dame Un número para sumarlo x 2 ");
-		Scanner teclado=new Scanner(System.in);
+		Scanner teclado =new Scanner(System.in);
 		num1 =teclado.nextInt();
 		int resultado= num1 * num2;
 		System.out.println("El resultado es: "+ resultado);
+
+		
+		Suma cSuma=new Suma();
+		System.out.println(cSuma.getSuma(2,3));
+	}
+
+	public int getSuma(int num1, int num2){
+		return num1+num2;
 	}
 }
-
+//puro loko
