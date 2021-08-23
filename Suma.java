cc79ee54d207ea054d148acf0f5a7c66788c8ec6
AnthonyTch <anthony.tepach@gmail.com>
2021-08-23 16:45:44 -0500

    Update Suma.java
    Optimización de librería

--- Suma.java
+++ Suma.java
@@ -1,15 +1,16 @@
-import java.util.*;
+//Importa solo la libreria(s) que vayas a usar en el momento.
+//optimiza tus recursos
+import java.util.Scanner;
 
 public class Suma{
-	public static void main(String args[])
-{
-	int num1=0;
-	int num2=2;
-	System.out.println("Dame Un número para sumarlo x 2 ");
-	Scanner teclado=new Scanner(System.in);
-	num1 =teclado.nextInt();
-	int resultado= num1 * num2;
-	System.out.println("El resultado es: "+ resultado);
+	public static void main(String args[]){
+		int num1=0;
+		int num2=2;
+		System.out.println("Dame Un número para sumarlo x 2 ");
+		Scanner teclado=new Scanner(System.in);
+		num1 =teclado.nextInt();
+		int resultado= num1 * num2;
+		System.out.println("El resultado es: "+ resultado);
 	}
 }
 
